402adb1b8bb604d2bcdbebe7fed48d647e20a2c5
Sertaç Kocagil <srtc.kcgl@gmail.com>
2023-04-20 11:24:02 +0300

    done flexible tasks

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it
+
+const server = require('./api/server')
+const port = process.env.PORT || 9000
+
+server.listen(port, () => {
+  console.log(`running on port: ${port}`)
+})
