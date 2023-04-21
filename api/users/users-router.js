a8d291379a97e3b5834a29ee26bdd1cecbf2e1b1
Sertaç Kocagil <srtc.kcgl@gmail.com>
2023-04-21 16:44:39 +0300

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,17 +1,23 @@
 const express = require('express')
 
 const middleware = require('../middleware/middleware')
+const userModel = require('./users-model')
 
 // `users-model.js` ve `posts-model.js` sayfalarına ihtiyacınız var
 // ara yazılım fonksiyonları da gereklidir
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // TÜM KULLANICILARI İÇEREN DİZİYİ DÖNDÜRÜN
+  try {
+    const users = await userModel.get()
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', middleware.validateUserId, (req, res) => {
   // USER NESNESİNİ DÖNDÜRÜN
   // user id yi getirmek için bir ara yazılım gereklidir
   try {
@@ -26,23 +32,23 @@ router.post('/', (req, res) => {
   // istek gövdesini doğrulamak için ara yazılım gereklidir.
 })
 
-router.put('/:id', (req, res) => {
+router.put('/:id', middleware.validateUserId, (req, res) => {
   // YENİ GÜNCELLENEN USER NESNESİNİ DÖNDÜRÜN
   // user id yi doğrulayan ara yazılım gereklidir
   // ve istek gövdesini doğrulayan bir ara yazılım gereklidir.
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', middleware.validateUserId, (req, res) => {
   // SON SİLİNEN USER NESNESİ DÖNDÜRÜN
   // user id yi doğrulayan bir ara yazılım gereklidir.
 })
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', middleware.validateUserId, (req, res) => {
   // USER POSTLARINI İÇEREN BİR DİZİ DÖNDÜRÜN
   // user id yi doğrulayan bir ara yazılım gereklidir.
 })
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', middleware.validateUserId, (req, res) => {
   // YENİ OLUŞTURULAN KULLANICI NESNESİNİ DÖNDÜRÜN
   // user id yi doğrulayan bir ara yazılım gereklidir.
   // ve istek gövdesini doğrulayan bir ara yazılım gereklidir.
